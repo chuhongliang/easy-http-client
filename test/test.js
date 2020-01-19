b04ba486127f2ef73c82e0b0fc9e173e10e42476
chuhongliang <13717807729@163.com>
2020-01-19 16:00:22 +0800

    增加https支持 对method验证

--- test/test.js
+++ test/test.js
@@ -1,6 +1,17 @@
 const { http, curl } = require('../lib/http');
 
-http.get('http://127.0.0.1:3000', { name: 'aa' })
+// http.get('http://127.0.0.1:3000', { name: 'aa' })
+// 	.then(function (res) {
+// 		console.log('res=>', res);
+// 	}).catch(function (err) {
+// 		console.log('err=>', err);
+// 	});
+
+let opts = {
+	method: 'post',
+	url: 'https://127.0.0.1:3000/v1/user'
+}
+curl(opts)
 	.then(function (res) {
 		console.log('res=>', res);
 	}).catch(function (err) {
